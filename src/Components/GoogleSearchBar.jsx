fa1563923099a9fd25ab42275fcb679cc9904f48
Noah Santos <111617611+Noah-Santos@users.noreply.github.com>
2024-01-09 15:08:05 -0700

    Search Bit
    Have the search bit figured out. Just need to implement it to make the whole site work

---
+++ src/Components/GoogleSearchBar.jsx
@@ -0,0 +1,11 @@
+// window.open(`http://www.google.com/search?q=${search}`, '_blank')
+
+import React from 'react'
+
+const GoogleSearchBar = () => {
+    return (
+        <div>GoogleSearchBar</div>
+    )
+}
+
+export default GoogleSearchBar;
