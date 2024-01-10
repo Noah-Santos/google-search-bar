834cbc4208f8e660267fb47d554bfe0da025b34d
Noah Santos <111617611+Noah-Santos@users.noreply.github.com>
2024-01-10 14:28:49 -0700

    Functional
    The search bar is now functional

--- src/Components/GoogleSearchBar.jsx
+++ src/Components/GoogleSearchBar.jsx
@@ -1,14 +1,22 @@
 // window.open(`http://www.google.com/search?q=${search}`, '_blank')
 
-import {useRef} from 'react'
+import {useRef} from 'react';
+import { IoMdSearch } from "react-icons/io";
 
 const GoogleSearchBar = () => {
-    function search(){
-
+    let input = useRef('');
+    
+    const search = (e)=>{
+        e.preventDefault();
+        console.log(input.current.value)
+        window.open(`http://www.google.com/search?q=${input.current.value}`, '_blank')
     }
     return (
         <div>
-            <form action={search}></form>
+            <form onSubmit={search}>
+                <input type="text" ref={input} placeholder='Search Google'/>
+                <button type='submit'><IoMdSearch /></button>
+            </form>
         </div>
     )
 }
